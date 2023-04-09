72136ddde33beb6f35541bc0a6745d38c1e2e0b8
prernadave <prernadave3914@gmail.com>
2023-04-09 21:42:34 +0530

    done frontend

--- FE/script/index.js
+++ FE/script/index.js
@@ -7,6 +7,14 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.display= "block";
 }
+let sec = document.getElementsByClassName('sec-1')
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+
+}
+
 
 
 

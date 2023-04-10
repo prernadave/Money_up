ba7762b6c384f84ef427c4e735245ec754ba2fa3
raj8888 <jadhavrj8877@gmail.com>
2023-04-10 13:20:11 +0530

    complete the update functionality,cancel button functionality , add sweetalerts instead of alerts

--- FE/script/budget.js
+++ FE/script/budget.js
@@ -98,7 +98,7 @@ let allTask=data.map((elem)=>{
             <p class="task-name-task">${elem.taskname}</p>
             <p class="task-prize-task">${elem.taskprize}₹</p>
             <div class="up-btn-div">
-            <button class="task-update-task">Update</button>
+            <button class="task-update-task" data-id=${elem._id}>Update</button>
             </div>
             <div class="up-btn-div">
             <button class="task-comp-task" 
@@ -121,7 +121,7 @@ compButton.forEach(elem=>{
             flag=true
         }
        }else{
-       alert("Task is already completed")
+        swal("Task is already completed","Your are not able to change the status of task.","error")
        }
         let obj={
             completed:flag
@@ -129,9 +129,85 @@ compButton.forEach(elem=>{
         updateData(taskID,obj)
     })
 })
+
+let getUpdattBtn=document.querySelectorAll(".task-update-task")
+getUpdattBtn.forEach(elem=>{
+    elem.addEventListener('click',(event)=>{
+      let taskID=event.target.dataset.id
+      getOneTask(taskID)
+     
+    })
+})
+
 }
 }
 
+async function getOneTask(id){
+    try {
+        let getTask=await fetch(`${baseURL}/newbudget/task/${id}`,{
+        method:"GET",
+        headers:{  
+            Authorization:sessionStorage.getItem("email")
+        },
+    })
+        if(getTask.ok){
+            let task=await getTask.json()
+            let taskData=task.task[0]
+            if(taskData.completed){
+               return await swal("Task is Already Completed","You are not able to update this task","error")
+            }else{
+                renderTaskForUpdate(taskData)
+            }
+        }else{
+            console.log(getTask)
+        }
+    } catch (error) {
+        alert('Server Error')
+        console.log(error.message)
+    }
+    
+}
+
+function renderTaskForUpdate(taskData){
+    let formrender=document.getElementById("budget-form")
+    formrender.innerHTML=`
+    <h1 id=create-taskt-title>Create a Task</h1>
+            <form id="task-creat-form">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRR7P5zOBSZxTrT-yxeyi8okKf6K3wu3ZijIw" alt="crate-task" id="create-task-img">
+                    <p id="crt-task-bank">Select Bank Account</p>
+                    <select name="accounts" id="crt-task-bank-acct" for="account">
+                        
+                         ${renderOptionsForUpdate(allAccounts,taskData)}
+                    </select>
+                    <p id="crt-task-name">Enter Task Name</p>
+                    <input type="text" id="crt-task-nameip" placeholder="Enter task title here..."  for="taskname" value="${taskData.taskname}" required>
+                    <p id="crt-task-prize">Enter Task Prize</p>
+                    <input type="number" id="crt-task-prizeip" placeholder="Enter task prize here..."  for="prize" value="${taskData.taskprize}" required>
+                    <button type="submit" id="crt-task-sbbtn">Submit</button>
+                    <button id="crt-task-cabtn">Cancel</button>
+                    
+            </form>
+    `
+
+    let cancelForUp=document.getElementById("crt-task-cabtn")
+    cancelForUp.addEventListener("click",(event)=>{
+        window.location.reload();
+    })
+    let formtask=document.querySelector("form")
+    formtask.addEventListener('submit',(event)=>{
+        event.preventDefault()
+        let bank=document.getElementById("crt-task-bank-acct").value
+        let taskname=document.getElementById("crt-task-nameip").value
+        let taskprize=document.getElementById("crt-task-prizeip").value
+        let obj={
+            bankID:bank,
+            taskname:taskname,
+            taskprize:taskprize,
+        }
+
+        updateCompTask(obj,taskData._id)
+    })
+}
 function selectColor(flag){
 // console.log('#ff5640')
 if(flag==false){
@@ -139,6 +215,28 @@ if(flag==false){
 }else{
     return '#23a455'
 }
+}
+async function  updateCompTask(obj,id){
+    try {
+        let updata=await fetch(`${baseURL}/newbudget/update/${id}`,{
+        method:"PATCH",
+        headers:{  
+            "Content-Type":"application/json", 
+            Authorization:sessionStorage.getItem("email")
+        },
+        body:JSON.stringify(obj)
+    })
+    if(updata.ok){
+         await swal("Task Updated Successfully","Thank You","success")
+        window.location.reload();
+        }else{
+            console.log(updata)
+        }
+    } catch (error) {
+        alert('Server Error')
+        console.log(error.message)
+    }
+    
 }
 
 async function updateData(id,obj){
@@ -151,9 +249,9 @@ try {
     },
     body:JSON.stringify(obj)
 })
-    if(updata.ok){
-        alert("Task Update Successfully!")
-        window.location.reload();
+if(updata.ok){
+     await swal("Task Completed.","Money Deducted from your bank account","success")
+    window.location.reload();
     }else{
         console.log(updata)
     }
@@ -205,6 +303,7 @@ async function createTask(obj){
             body:JSON.stringify(obj)
         })
         if(fetchingData.ok){
+            await swal("Task Created Successfully","Thank You!","success")
             window.location.reload();
         }else{
           console.log("Server Error")
@@ -245,6 +344,29 @@ function renderOptions(data){
     `
    })
    return temp.join("")
+}
+
+function renderOptionsForUpdate(allAccounts,taskData){
+let main=[]
+let temp=allAccounts.filter(elem=>{
+    if(elem._id==taskData.bankID){
+        return elem
+    }
+})
+
+let temp2=allAccounts.filter(elem=>{
+    if(elem._id!=taskData.bankID){
+        return elem
+    }
+})
+
+main=[...temp,...temp2]
+let temp3=main.map(elem=>{
+    return `
+    <option value="${elem._id}">${elem.name}</option>
+    `
+   })
+return temp3.join("")
 }
 
 function findBankAccount(bankID,banks){
